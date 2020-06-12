061c38da1fa3f9b4efd5d2f99a5a4bdd1ecbd253
roman89ua <roman89ua@gmail.com>
2020-06-12 21:20:09 +0300

    some

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {PictureService} from '../picture.service';
-import {infoAbout} from '../app.constants';
+import { PictureService } from '../picture.service';
+import { infoAbout } from '../app.constants';
 
 // import {imgUrl, facebookUrl} from '../app.constants';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  
 
-  // image = imgUrl ;
-  // facebook = facebookUrl;
-  public image: string ;
-  public facebook: string ;
-  public infoAbout: string [] ;
-  constructor(private pictureService: PictureService ) { 
+    // image = imgUrl ;
+    // facebook = facebookUrl;
+    public image: string;
+    public facebook: string;
+    public infoAbout: string[];
+    constructor(private pictureService: PictureService) {
 
-  }
+    }
 
-  ngOnInit() {
-    this.image = this.pictureService.image;
-    this.facebook = this.pictureService.facebook;
-    this.infoAbout = this.pictureService.infoAbout;
-  }
+    ngOnInit() {
+        this.image = this.pictureService.image;
+        this.facebook = this.pictureService.facebook;
+        this.infoAbout = this.pictureService.infoAbout;
+    }
 
 }
--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -3,28 +3,28 @@ import { PictureService } from '../picture.service';
 import { CurrencyInterface } from '../currency-interface';
 
 @Component({
-  selector: 'app-currency',
-  templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.css']
+    selector: 'app-currency',
+    templateUrl: './currency.component.html',
+    styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
-  currensyList: CurrencyInterface[] = [];
-  loading = false;
-  displayedColumns = ['txt', 'cc', 'rate'];
+    currensyList: CurrencyInterface[] = [];
+    loading = false;
+    displayedColumns = ['txt', 'cc', 'rate'];
 
-  constructor(private pictureService: PictureService ){}
+    constructor(private pictureService: PictureService) { }
 
-  ngOnInit() {
-    this.loading = true;
-    this.pictureService.getUrl()
-    .subscribe((data: any) => {
-      let url = data.currencyUrl;
-      this.pictureService.getCurrency(url)
-      .subscribe((cur: CurrencyInterface[])=>{
-        this.currensyList = cur;
-        this.loading = false;
-        
-      });
-    });
-  }
+    ngOnInit() {
+        this.loading = true;
+        this.pictureService.getUrl()
+            .subscribe((data: any) => {
+                const url = data.currencyUrl;
+                this.pictureService.getCurrency(url)
+                    .subscribe((cur: CurrencyInterface[]) => {
+                        this.currensyList = cur;
+                        this.loading = false;
+
+                    });
+            });
+    }
 }
